66d5221d5a178cf557d5f10bc4970d5113baab57
Arthri <41360489+a@users.noreply.github.com>
2023-11-25 16:11:11 +0000

    Remove reduce

--- tgui/packages/common/collections.js
+++ tgui/packages/common/collections.js
@@ -1,24 +1,4 @@
 /**
- * A fast implementation of reduce.
- */
-export const reduce = (reducerFn, initialValue) => (array) => {
-  const length = array.length;
-  let i;
-  let result;
-  if (initialValue === undefined) {
-    i = 1;
-    result = array[0];
-  } else {
-    i = 0;
-    result = initialValue;
-  }
-  for (; i < length; i++) {
-    result = reducerFn(result, array[i], i, array);
-  }
-  return result;
-};
-
-/**
  * Creates an array of grouped elements, the first of which contains
  * the first elements of the given arrays, the second of which contains
  * the second elements of the given arrays, and so on.
--- tgui/packages/common/vector.js
+++ tgui/packages/common/vector.js
@@ -1,4 +1,4 @@
-import { reduce, zip } from './collections';
+import { zip } from './collections';
 
 /**
  * Creates a vector, with as many dimensions are there are arguments.
@@ -16,19 +16,19 @@ const MUL = (a, b) => a * b;
 const DIV = (a, b) => a / b;
 
 export const vecAdd = (...vecs) => {
-  return reduce((a, b) => zip(a, b).map((v) => ADD(...v)))(vecs);
+  return zip(...vecs).map((v) => v.reduce(ADD, 0));
 };
 
 export const vecSubtract = (...vecs) => {
-  return reduce((a, b) => zip(a, b).map((v) => SUB(...v)))(vecs);
+  return zip(...vecs).map((v) => v.reduce(SUB, 0));
 };
 
 export const vecMultiply = (...vecs) => {
-  return reduce((a, b) => zip(a, b).map((v) => MUL(...v)))(vecs);
+  return zip(...vecs).map((v) => v.reduce(MUL, 0));
 };
 
 export const vecDivide = (...vecs) => {
-  return reduce((a, b) => zip(a, b).map((v) => DIV(...v)))(vecs);
+  return zip(...vecs).map((v) => v.reduce(DIV, 0));
 };
 
 export const vecScale = (vec, n) => {
@@ -40,7 +40,7 @@ export const vecInverse = (vec) => {
 };
 
 export const vecLength = (vec) => {
-  return Math.sqrt(reduce(ADD)(vecMultiply(vec, vec)));
+  return Math.sqrt(vecMultiply(vec, vec).reduce(ADD, 0));
 };
 
 export const vecNormalize = (vec) => {
