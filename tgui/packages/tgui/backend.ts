a2c84ecaad59797978f031a04dd41986dbda8c9d
Arthri <41360489+a@users.noreply.github.com>
2025-07-19 10:21:32 +0000

    Restore scale

--- tgui/packages/tgui/backend.ts
+++ tgui/packages/tgui/backend.ts
@@ -378,6 +378,7 @@ type BackendState<TData> = {
       size: [number, number];
       fancy: BooleanLike;
       locked: BooleanLike;
+      scale: BooleanLike;
     };
     client: {
       ckey: string;
